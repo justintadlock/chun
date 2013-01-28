731134a47703c508138ea62e94bdc9e596cf6490
Justin Tadlock <justin@justintadlock.com>
2013-01-28 01:18:46 -0600

    That extra space is bothering me. :)

--- header.php
+++ header.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-
 <head>
 
 <meta http-equiv="Content-Type" content="<?php bloginfo( 'html_type' ); ?>; charset=<?php bloginfo( 'charset' ); ?>" />
